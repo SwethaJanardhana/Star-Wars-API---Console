ff8f44196327b4b87cbcc4e26f4d03077ae0000c
Swetha Janardhana <SwethaJanardhana85@gmail.com>
2024-01-23 10:21:45 +0100

    Task accomplished

--- js/index.js
+++ js/index.js
@@ -2,6 +2,21 @@ console.clear();
 
 const url = "https://swapi.dev/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  const response = await fetch(url);
+  if (response.status === 200) {
+    const data = await response.json();
+    const characters = data.results;
+    characters.forEach((character) => console.table(character));
+    const characterNames = characters.map((character) => character.name);
+    console.log("Names: ", characterNames);
+    const requiredObject = characters.find(
+      (character) => character.name === "R2-D2"
+    );
+    console.log(
+      `${requiredObject.name} eye color is : ${requiredObject.eye_color}`
+    );
+  }
+}
 
 fetchData();
